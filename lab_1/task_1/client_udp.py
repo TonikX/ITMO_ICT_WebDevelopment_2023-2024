627633bff5bef43ff015c2bbc9006b11e3ed8e0c
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2023-09-30 12:59:46 +0300

    First and second Tasks

---
+++ lab_1/task_1/client_udp.py
@@ -0,0 +1,12 @@
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 44455
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+message = "Hello, server"
+client_socket.sendto(message.encode(), (SERVER_IP, SERVER_PORT))
+
+data, addr = client_socket.recvfrom(1024)
+print(f"Получено сообщение от {addr}: {data.decode()}")
---
+++ lab_1/task_2/client_tcp.py
@@ -0,0 +1,17 @@
+import socket
+
+IP = "127.0.0.1"
+PORT = 44455
+codage = 'utf-8'
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+
+inp_string = input("Enter the length of two legs: ").encode(codage)
+client_socket.send(inp_string)
+
+result = client_socket.recv(1024).decode(codage)
+
+print(f"Hypotenuse length: {result}")
+
+client_socket.close()
