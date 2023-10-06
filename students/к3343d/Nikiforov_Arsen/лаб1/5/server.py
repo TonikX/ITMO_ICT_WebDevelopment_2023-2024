bfccf7d71b2112c72ff16bb7b9851164f614a82a
Arsenshmid <apce2@mail.ru>
2023-10-07 01:17:13 +0300

    задеплоил документацию ссылка вот https://arsenshmid.github.io/ITMO_ICT_WebDevelopment_2023-2024/

--- students/к3343d/Nikiforov_Arsen/лаб1/5/server.py
+++ students/к3343d/Nikiforov_Arsen/лаб1/5/server.py
@@ -1,7 +1,6 @@
 ﻿import http.server
 import urllib.parse
 
-# Запись оценок
 grades = {}
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
@@ -10,21 +9,27 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            # HTML-форма для добавления оценки
+
             html = """
-                <html>
-                <head><title>Add Grade</title></head>
-                <body>
-                <h1>Add Grade</h1>
-                <form method="POST" action="/add_grade">
-                    <label for="subject">Subject:</label><br>
-                    <input type="text" id="subject" name="subject"><br>
-                    <label for="grade">Grade:</label><br>
-                    <input type="number" id="grade" name="grade" min="1" max="5"><br><br>
-                    <input type="submit" value="Submit">
-                </form>
-                <p><a href="/grades">Grades Overview</a></p>
-                </body></html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Grade</title>
+
+</head>
+<body>
+    <h1>Add Grade</h1>
+    <form method="POST" action="/add_grade">
+        <label for="subject">Subject:</label><br>
+        <input type="text" id="subject" name="subject"><br>
+        <label for="grade">Grade:</label><br>
+        <input type="number" id="grade" name="grade" min="1" max="5"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+    <p><a href="/grades">Grades Overview</a></p>
+</body>
+</html>
+
             """
             self.wfile.write(bytes(html, "utf8"))
             return
@@ -32,24 +37,25 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            
-            # Страница с обзором оценок в виде таблицы
+
+            # Формируем HTML-таблицу для обзора оценок
             html = """
-                <html>
-                <head><title>Grades Overview</title></head>
-                <body>
-                <h1>Grades Overview</h1>
-                <table border="1">
-                <tr>
-                    <th>Subject</th>
-                    <th>Grade</th>
-                </tr>
+            
+            <html>
+            <head>
+            <title>Grades Overview</title>
+            </head>
+            
+            <body>
+            <h1>Grades Overview</h1>
+            <table border="1">
+            <tr><th>Subject</th>
+            <th>Grades</th></tr>
             """
-            for subject, grade in grades.items():
-                html += f"<tr><td>{subject}</td><td>{grade}</td></tr>"
-            html += "</table>"
-            html += "<p><a href='/'>Add Grade</a></p>"
-            html += "</body></html>"
+            for subject, subject_grades in grades.items():
+                # Добавляем строки в таблицу для каждого предмета и его оценок
+                html += f"<tr><td>{subject}</td><td>{', '.join(map(str, subject_grades))}</td></tr>"
+            html += "</table><p><a href='/'>Add Grade</a></p></body></html>"
             self.wfile.write(bytes(html, "utf8"))
             return
 
@@ -59,11 +65,15 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         params = urllib.parse.parse_qs(post_data)
         subject = params['subject'][0]
         grade = params['grade'][0]
+
         try:
             grade = int(grade)
             if grade < 1 or grade > 5:
                 raise ValueError("Grade out of range")
-            grades[subject] = grade
+            if subject not in grades:
+                grades[subject] = []
+            grades[subject].append(grade)
+
             self.send_response(303)
             self.send_header('Location', '/')
             self.end_headers()
@@ -72,10 +82,8 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes("Bad request. Grade must be between 1 and 5.", "utf8"))
 
-# Создание объекта обработчика запросов
 handler_object = MyHttpRequestHandler
-# Создание HTTP-сервера
 my_server = http.server.HTTPServer(("localhost", 8080), handler_object)
+
 print("Server is running on localhost:8080...")
-# Запуск сервера
 my_server.serve_forever()
