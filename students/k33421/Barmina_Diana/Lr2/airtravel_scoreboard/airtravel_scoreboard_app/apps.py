55b3bbdd6c61996d3856bc29b6ea28890e9b9bd4
Diana <diana.barmina.2003@mail.ru>
2023-10-27 00:17:12 +0300

    Add lab

---
+++ students/k33421/Barmina_Diana/Lr2/airtravel_scoreboard/airtravel_scoreboard_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AirtravelScoreboardAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "airtravel_scoreboard_app"
