00ec08ecdf90d3838187999d25a1235ac32acb16
Nikita <tyumin000@gmail.com>
2023-10-26 13:27:31 +0300

    Выполнена практическая работа 3

--- students/k33402/practical_works/Tiumin_Nikita/simple_django_web_project/blogfspo/blog/models.py
+++ students/k33402/practical_works/Tiumin_Nikita/simple_django_web_project/blogfspo/blog/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 
-class Owner(models.Model):
+class Owner(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     birth = models.DateTimeField(blank=True, null=True)
+
+    passport = models.CharField(max_length=8, blank=True)
+    address = models.CharField(max_length=50, blank=True)
+    nationality = models.CharField(max_length=30, blank=True)
+
+    username = models.CharField(max_length=30, blank=True, unique=True, null=True)
 
     cars = models.ManyToManyField('Car', through='CarOwnership')
 
 
 class DriverLicense(models.Model):
-    owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     number = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
     issued_at = models.DateTimeField()
@@ -26,7 +34,7 @@ class Car(models.Model):
 
 
 class CarOwnership(models.Model):
-    owner_id = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    owner_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     car_id = models.ForeignKey(Car, on_delete=models.CASCADE)
     starts_at = models.DateTimeField()
     ends_at = models.DateTimeField(blank=True, null=True)
