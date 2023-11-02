a62708ef15eeb699591b7f0cb3c63a13111b7d1b
Mesh296 <minhhan181@gmail.com>
2023-11-02 15:02:05 +0300

    feat: add practical work 2

---
+++ students/K33392/Tran_Duc_Minh/practical_works/simple_django_web_project/practical_work_2_1/django_first_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Owner(models.Model):
+    id_owner = models.AutoField(primary_key=True)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    date_of_birth = models.DateTimeField()
+
+class Car(models.Model):
+    id_car = models.AutoField(primary_key=True)
+    state_number = models.CharField(max_length=15)
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=30)
+    owners = models.ManyToManyField(Owner, through='Own')
+
+class Own(models.Model):
+    id_owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    id_car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    date_star = models.DateTimeField()
+    date_end = models.DateTimeField()
+
+class Driver_license(models.Model):
+    id_license = models.AutoField(primary_key=True)
+    id_owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+    date_issued = models.DateField()
