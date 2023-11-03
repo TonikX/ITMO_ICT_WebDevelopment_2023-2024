3b285a77883370b1cd1b14a3e79e522ff7a34092
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-11-03 12:33:48 +0300

    feat: created template and view for car owners

--- students/K33392/practical_works/Рыбалко_Олег/simple_django_web_project/django_project_rybalko/project_first_app/views.py
+++ students/K33392/practical_works/Рыбалко_Олег/simple_django_web_project/django_project_rybalko/project_first_app/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import render
+
+from .models import CarOwner
+
+
+def car_owner(request: HttpRequest, owner_id: str) -> HttpResponse:
+    try:
+        owner = CarOwner.objects.get(pk=owner_id)
+    except CarOwner.DoesNotExist:
+        raise Http404("owner doesn't exist")
+
+    return render(request, "car_owner/detail.html", dict(owner=owner))
