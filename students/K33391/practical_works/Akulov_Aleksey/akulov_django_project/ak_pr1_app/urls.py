1b12780adab789f6c2fa38c65142374308b07d8b
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2023-11-04 21:28:58 +0300

    practice-2-finished

--- students/K33391/practical_works/Akulov_Aleksey/akulov_django_project/ak_pr1_app/urls.py
+++ students/K33391/practical_works/Akulov_Aleksey/akulov_django_project/ak_pr1_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from . import views
+from .views import *
+
 urlpatterns = [
-        path('owner/<int:owner_id>/', views.detail)
+        path('owner/<int:owner_id>/', detail),
+        path("cars", CarListView.as_view()),
+        path("cars/<int:pk>", CarDetailView.as_view()),
+        path("cars/<int:pk>/update", CarUpdateView.as_view()),
+        path("cars/<int:pk>/delete", CarDeleteView.as_view()),
+        path("cars/create", CarCreateView.as_view()),
+        path("owners", owner_list, name = "owner_list"),
+        path("owners/<int:owner_id>", owner_get),
+        path("owners/create", owner_create)
 ]
 
