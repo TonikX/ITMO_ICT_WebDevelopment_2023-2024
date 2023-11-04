af8e50c8b5052e3aa85f2b96d52d8ad4c5e4eaea
Your NameAkulov_Aleksei <ackulow.ak@yandex.ru>
2023-11-04 17:23:20 +0300

    practice-1-finished

---
+++ students/K33391/practical_works/Akulov_Aleksey/akulov_django_project/ak_pr1_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class CarOwner(models.Model):
+    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    birth_date = models.DateTimeField(null=True, blank=True)
+
+
+class DrivingLicense(models.Model):
+    owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
+    license_number = models.CharField(max_length=10)
+    type = models.CharField(max_length=10)
+    issue_date = models.DateTimeField()
+
+
+class Car(models.Model):
+    state_number = models.CharField(max_length=15)
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
+    color = models.CharField(max_length=30, null=True, blank=True)
+
+
+class Ownership(models.Model):
+    owner = models.ForeignKey(CarOwner, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(null=True, blank=True)
+
