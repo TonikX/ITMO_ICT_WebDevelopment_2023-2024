77dbf6ff29161e5fcfda609ead844f7d634ef127
mynameislyonya <90193660+Lbabichev17@users.noreply.github.com>
2023-11-06 15:38:09 +0300

    lab2

---
+++ students/k33422/leonid_babichev/lab2/tours/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from users.models import Tourist
+
+
+class Tour(models.Model):
+    name = models.CharField(max_length=300)
+    destination = models.CharField(max_length=200)
+    country = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    tourists = models.ManyToManyField(Tourist, through='Reservation')
+
+
+class Reservation(models.Model):
+    tourist = models.ForeignKey(Tourist, on_delete=models.DO_NOTHING)
+    tour = models.ForeignKey(Tour, on_delete=models.DO_NOTHING)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_confirmed = models.BooleanField(default=False)
+
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    reservation = models.ForeignKey(Reservation, on_delete=models.DO_NOTHING)
+    comment = models.CharField(max_length=1000)
