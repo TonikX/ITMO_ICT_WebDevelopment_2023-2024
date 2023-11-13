3f42637b61fd21b8fd2244e7625f13a4924cd252
”Alice <alice.mikulina@gmail.com>
2023-11-13 15:07:20 +0300

    fixed task 5 for lab1

--- students/K33421/laboratory_works/Mikulina_Alice/laboratory_work_1/tsk5/server.py
+++ students/K33421/laboratory_works/Mikulina_Alice/laboratory_work_1/tsk5/server.py
@@ -1,66 +1,82 @@
 import socket
-import sys
+import urllib.parse 
+ 
+ 
+ 
+class MyHTTPServer: 
+    def __init__(self, host, port): 
+        self.host = host 
+        self.port = port 
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.sock.bind((self.host, self.port)) 
+        self.sock.listen(1) 
+        self.grades = {} 
+ 
+    def serve_forever(self): 
+        print(f"Server started...") 
+        while True: 
+            client, addr = self.sock.accept() 
+            self.serve_client(client) 
+ 
+    def serve_client(self, client): 
+        data = client.recv(1024).decode("utf-8") 
+        self.parse_request(client, data) 
+ 
+    def parse_request(self, client, data): 
+        lines = data.split("\n") 
+        if len(lines) < 1: 
+            raise HTTPError(400, 'Bad request') 
+ 
+        method, url, ver = lines[0].split() 
+        parsed_url = urllib.parse.urlparse(url) 
 
-class MyHTTPServer:
-
-    def __init__(self, host: str, port: int):
-        self.host = host
-        self.port = port
-        self.connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect.bind((host, port))
-        self.connect.listen(1)
-        self.grades = {}
-  
-
-    def serve_forever(self):
-         while True:
-            client, address = self.connect.accept()
-            self.serve_client(client)
-
-
-    def serve_client(self, client):
-        data = client.recv(16384).decode()
-        self.parse_request(client, data)
-
-
-    def parse_request(self, client, data):
-        lines = data.split("\n")
-        method, url, version = lines[0].split()
-        params = None
-
-        if "?" in url:
-            param_list = url.split("?")[1].split("&")
-            params = {p.split("=")[0]: p.split("=")[1] for p in param_list}
+        if ver != 'HTTP/1.1': 
+            raise HTTPError(505, 'HTTP Version Not Supported') 
+        
+        param_list = parsed_url.query.replace('=', ':').split('&') 
+ 
+        params = {} 
+        for item in param_list: 
+            item = item.split(':') 
+            if len(item) > 1: 
+              params[item[0]] = item[1] 
+        
         self.handle_request(client, method, params)
 
-
     def handle_request(self, client, method, params):
-        if method == 'POST':
+        if method == 'GET':
+            self.send_response(client, 200, "OK", self.grades_to_html_page())
+        elif method == 'POST':
             disciplines = params.keys()
             for discipline in disciplines:
                 grade = params[discipline]
-                self.grades[discipline] = grade
+                if discipline in self.grades:
+                    self.grades[discipline] += f",{grade}"
+                else:
+                    self.grades[discipline] = grade
             self.send_response(client, 200, "OK", "Data saved")
-        elif method == 'GET':
-            self.send_response(client, 200, "OK", self.grades_to_html_page())
         else:
-            self.send_response(client, 404, "Not Found", "Wrong method.")
-
+            raise HTTPError(404, 'Not Found', 'Method must be in ("GET", "POST")') 
 
     def send_response(self, client, code, reason, body):
         response = f"HTTP/1.1 {code} {reason}\nContent-Type: text/html\n\n{body}"
-        client.send(response.encode())
+        client.send(response.encode("utf-8"))
         client.close()
 
-
     def grades_to_html_page(self):
-        page = "<html><body><ul>"
-        for discipline, grade in self.grades.items():
-            page += f"<li>{discipline}: {grade}"
-        page += "</ul></body></html>"
-        return page
+        page = "<html><body><ul>" 
+        for discipline, grade in self.grades.items(): 
+            page += f"<li>{discipline}: {grade}" 
+        page += "</ul></body></html>" 
+        return page 
 
 
+class HTTPError(Exception): 
+    def __init__(self, status, reason, body=None): 
+        super() 
+        self.status = status 
+        self.reason = reason 
+        self.body = body
 
 
 if __name__ == '__main__':
@@ -71,4 +87,3 @@ if __name__ == '__main__':
         serv.serve_forever()
     except KeyboardInterrupt:
         serv.connect.close()
-
