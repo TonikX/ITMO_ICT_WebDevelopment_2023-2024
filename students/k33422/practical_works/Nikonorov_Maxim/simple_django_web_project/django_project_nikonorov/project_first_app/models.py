001a6291dd2a55569f4f74078a9e037072f45f8a
MaksNick <nic50@list.ru>
2023-11-29 15:52:24 +0300

    added practice 3.1

--- students/k33422/practical_works/Nikonorov_Maxim/simple_django_web_project/django_project_nikonorov/project_first_app/models.py
+++ students/k33422/practical_works/Nikonorov_Maxim/simple_django_web_project/django_project_nikonorov/project_first_app/models.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import AbstractUser
 class CarOwner(AbstractUser):
     LastName = models.CharField(max_length=30)
     FirstName = models.CharField(max_length=30)
-    DateOfBirth = models.DateTimeField(null=True, blank=True)
+    DateOfBirth = models.DateField(null=True, blank=True)
     PassportNumber = models.CharField(max_length=30, null=True)
     Address = models.CharField(max_length=100, null=True)
     Nationality = models.CharField(max_length=50, null=True)
     
     def __str__(self):
-        return self.LastName + ' ' + self.FirstName
+        return 'owner ' + self.LastName + ' ' + self.FirstName
 
 class Car(models.Model):
     Number = models.CharField(max_length=15)
@@ -22,19 +22,19 @@ class Car(models.Model):
         return self.Brand + ' ' + self.Model
     
 class Ownership(models.Model):
-    carowner = models.ForeignKey('CarOwner', null=True, on_delete=models.CASCADE)
-    car = models.ForeignKey('Car', null=True, on_delete=models.CASCADE)
-    BeginDate = models.DateTimeField()
-    EndDate = models.DateTimeField(null=True, blank=True)
+    carowner = models.ForeignKey('CarOwner', null=True, on_delete=models.CASCADE, related_name='onwerships')
+    car = models.ForeignKey('Car', null=True, on_delete=models.CASCADE, related_name='onwerships')
+    BeginDate = models.DateField()
+    EndDate = models.DateField(null=True, blank=True)
     
     def __str__(self):
         return str(self.car) + ' | ' + str(self.carowner)
     
 class DriversLicense(models.Model):
-    carowner = models.ForeignKey('CarOwner', on_delete=models.CASCADE)
+    carowner = models.ForeignKey('CarOwner', on_delete=models.CASCADE, related_name='licenses')
     No = models.CharField(max_length=10)
     Type = models.CharField(max_length=10)
-    IssueDate = models.DateTimeField()
+    IssueDate = models.DateField()
     
     def __str__(self):
         return self.Type + ' | ' + str(self.carowner)
