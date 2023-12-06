b8d986daa4a5af3a837c602946a42623b32e9754
Tsimintiya Nikolay <wwwwat@inbox.ru>
2023-12-06 18:16:16 +0300

    objects registered to admin

--- students:K33392/laboratory_works:Tsimintiya_Nickolay/laboratory_work_2/warriors_app/admin.py
+++ students:K33392/laboratory_works:Tsimintiya_Nickolay/laboratory_work_2/warriors_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from models import Warrior, Skill, SkillOfWarrior, Profession
 # Register your models here.
+admin.site.register(Warrior)
+admin.site.register(Skill)
+admin.site.register(SkillOfWarrior)
+admin.site.register(Profession)
