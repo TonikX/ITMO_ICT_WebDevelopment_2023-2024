050e20759100988eeb6ba5168d661d86c0cc1408
Tsimintiya Nikolay <wwwwat@inbox.ru>
2023-12-06 18:06:30 +0300

    warriorAPIView added

--- students:K33392/laboratory_works:Tsimintiya_Nickolay/laboratory_work_2/warriors_app/views.py
+++ students:K33392/laboratory_works:Tsimintiya_Nickolay/laboratory_work_2/warriors_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Warrior
+from .serialazer import WarriorSerialazer
 # Create your views here.
+
+class WarriorAPIView(APIView):
+    def get(self, request):
+        warriors = Warrior.objects.all()
+        serializer = WarriorSerialazer(warriors, many=True)
+
+        return Response({"Warriors": serializer.data})
