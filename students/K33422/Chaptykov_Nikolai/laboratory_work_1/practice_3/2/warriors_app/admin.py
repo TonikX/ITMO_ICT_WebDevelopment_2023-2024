36f4f304ca00d2f64fe49afea41cb5f5cc134363
Николай Чаптыков <nick.chaptykov@gmail.com>
2023-12-11 06:23:29 +0300

    Перепутал директории, правлю чтобы было красиво

--- students/K33422/Chaptykov_Nikolai/laboratory_work_1/practice_3/2/warriors_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from warriors_app.models import *
-
-admin.site.register(Warrior)
-admin.site.register(Profession)
-admin.site.register(Skill)
-admin.site.register(SkillOfWarrior)
