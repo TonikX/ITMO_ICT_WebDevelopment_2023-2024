5dc86fbb1058f3ca3d9c4f843cc77b0f6299f3d0
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2023-12-11 20:44:52 +0300

    lab-3 models

---
+++ students/K33392/laboratory_works/Рыбалко_Олег/laboratory_work_3/blog/blog_app/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+  bio = models.TextField(blank=True)
+  profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+
+
+class Post(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  likes = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+
+class Comment(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Follow(models.Model):
+  follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+  followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+  created_at = models.DateTimeField(auto_now_add=True)
