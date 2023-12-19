f75e9d97786137c5dff74f2e09bbc70867f34fc6
Arsenshmid <apce2@mail.ru>
2023-12-19 07:47:18 +0300

    добавил стили

--- students/к3343d/labs/Nikiforov_Arsen/лаб3/hotel_management/hotel_management/settings.py
+++ students/к3343d/labs/Nikiforov_Arsen/лаб3/hotel_management/hotel_management/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'drf_yasg',
+
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
+# Путь к статическим файлам (CSS, JavaScript, Images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'hotel_api', 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
