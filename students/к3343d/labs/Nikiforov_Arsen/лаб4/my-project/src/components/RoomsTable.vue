f6a0f615e0803fa58a1778a90ccbb5978e6606b2
Arsenshmid <apce2@mail.ru>
2023-12-28 17:29:03 +0300

    добавлена фильтрация таблицы в клиенте

--- students/к3343d/labs/Nikiforov_Arsen/лаб4/my-project/src/components/RoomsTable.vue
+++ students/к3343d/labs/Nikiforov_Arsen/лаб4/my-project/src/components/RoomsTable.vue
@@ -1,21 +1,45 @@
-<template>
+<template> 
   <div>
     <h2>Список комнат</h2>
+    <label for="room_type">Room Type:</label>
+    <select v-model="filterType">
+      <option value="">--Select a Type--</option>
+      <option value="single">Single</option>
+      <option value="double">Double</option>
+      <option value="suite">Suite</option>
+    </select>
+
+    <label for="room_status">Room Status:</label>
+    <select v-model="filterStatus">
+      <option value="">--Select a Status--</option>
+      <option value="available">Available</option>
+      <option value="occupied">Occupied</option>
+      <option value="cleaning">Cleaning</option>
+    </select>
+
+    
+
     <table>
       <thead>
         <tr>
-          <th>Тип комнаты</th>
-          <th>Этаж</th>
-          <th>Статус</th>
-          <th>Стоимость</th>
+          <th>Room Type</th>
+          <th>Floor Number</th>
+          <th>Status</th>
+          <th>Cost</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="room in rooms" :key="room.id">
+        <tr v-for="room in filteredRooms" :key="room.id">
           <td>{{ room.room_type }}</td>
-          <td>{{ room.floorNumber }}</td>
+          <td>{{ room.floor.number }}</td>
           <td>{{ room.status }}</td>
           <td>{{ room.cost }}</td>
+          <td>
+            <button v-if="room.status === 'available'" @click="bookRoom(room.id)">
+              Book
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -28,24 +52,42 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      rooms: []
+      rooms: [],
+      filterType: '',
+      filterStatus: ''
     };
+  },
+  computed: {
+    filteredRooms() {
+      return this.rooms.filter(room => {
+        return (!this.filterType || room.room_type === this.filterType) &&
+               (!this.filterStatus || room.status === this.filterStatus);
+      });
+    }
   },
   created() {
     this.fetchRooms();
   },
   methods: {
     fetchRooms() {
-      axios.get('http://localhost:8000/hotel_api/api/rooms/')
+      axios.get('/hotel_api/api/rooms/')
         .then(response => {
-          this.rooms = response.data.map(room => ({
-            ...room,
-            floorNumber: room.floor ? room.floor.number : 'Неизвестно'
-          }));
-          console.log('Комнаты загружены:', this.rooms);
+          this.rooms = response.data;
         })
         .catch(error => {
-          console.error('Ошибка при загрузке данных о комнатах:', error);
+          console.error('Error loading rooms:', error);
+        });
+    },
+    applyFilter() {
+      this.fetchRooms();
+    },
+    bookRoom(roomId) {
+      axios.post(`/hotel_api/api/book_room/${roomId}`)
+        .then(() => {
+          this.fetchRooms(); // обновить после брони
+        })
+        .catch(error => {
+          console.error('Error booking room:', error);
         });
     }
   }
