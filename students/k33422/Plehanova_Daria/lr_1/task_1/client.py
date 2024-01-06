3ba6e928cb4908ecc4923149ed0519f206fe4af4
Даша <plehashadd@gmail.com>
2024-01-06 16:19:55 +0300

    task_1

---
+++ students/k33422/Plehanova_Daria/lr_1/task_1/client.py
@@ -0,0 +1,6 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as conn:
+    conn.sendto(b"Hello, server!", ('127.0.0.1', 8888))
+    data, addr = conn.recvfrom(1024)
+    print(f"Received from {addr}: {data.decode()}")
