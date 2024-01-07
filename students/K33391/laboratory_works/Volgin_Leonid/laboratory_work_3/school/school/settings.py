93372988c89ecee6abaa45ae858532e90f8c4e56
leoni <leonidvolgin2003@gmail.com>
2024-01-07 22:42:00 +0300

    LAB-4

--- students/K33391/laboratory_works/Volgin_Leonid/laboratory_work_3/school/school/settings.py
+++ students/K33391/laboratory_works/Volgin_Leonid/laboratory_work_3/school/school/settings.py
@@ -137,9 +137,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 CORS_ORIGIN_WHITELIST = (
-    'http//:localhost:8000',
+    'http://localhost:8000',
+    'http://localhost:5173',
+    'http://localhost:3000',
+
 )
