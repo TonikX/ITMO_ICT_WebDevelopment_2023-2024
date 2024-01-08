8ea4bdd4b253cd41a4f0d509f2ebac876f5b94b1
Seliarna <foti1516@gmail.com>
2024-01-08 09:58:13 +0300

    Update settings.py

--- students/K33392/laboratory_works/zlotnikova_karina/laboratory_work_3/lab3/settings.py
+++ students/K33392/laboratory_works/zlotnikova_karina/laboratory_work_3/lab3/settings.py
@@ -8,8 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uoyl4etj3j)y0^l@u_!d(%=49&#$5vnd3eakeqf0!c0(l(h1e='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
