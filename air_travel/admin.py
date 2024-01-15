36c724b1ac02d5c2c4e8b81aa74498d657779d10
KorolevaEkaterina334740 <trina696queen@gmail.com>
2024-01-15 22:35:47 +0300

    Revert "Лабораторная работа № 2"
    This reverts commit 3492653d4a4339c52d7acbdeb33dd1be8f1d3ea1.

--- air_travel/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .forms import NewPassengerForm
-from .models import CustomUser, Flight, Booking
-
-admin.site.register(CustomUser)
-admin.site.register(Flight)
-admin.site.register(Booking)
