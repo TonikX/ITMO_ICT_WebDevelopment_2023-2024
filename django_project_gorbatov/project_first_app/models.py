ea0291559e8e084b498aab98d0ab805a44a1d09b
godr1se <90646570+godr1se@users.noreply.github.com>
2024-02-26 12:06:18 +0300

    LR3+PR3

--- django_project_gorbatov/project_first_app/models.py
+++ django_project_gorbatov/project_first_app/models.py
@@ -1,28 +1,25 @@
 from django.db import models
 
 class Car(models.Model):
-    id_car = models.IntegerField(primary_key=True)
     state_number = models.CharField(max_length=15)
-    car_brand = models.CharField(max_length=20)
-    car_model = models.CharField(max_length=20)
+    brand = models.CharField(max_length=20)
+    model = models.CharField(max_length=20)
     color = models.CharField(max_length=30, null=True)
 
 class Owner(models.Model):
-    id_owner = models.IntegerField(primary_key=True)
     surname = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     birth_day = models.DateTimeField(null=True)
 
 class Own(models.Model):
-    id_car_owner = models.IntegerField(primary_key=True)
-    id_owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=True)
-    id_car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, null=True)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, null=True)
     date_start = models.DateTimeField()
     date_end = models.DateTimeField(null=True)
 
 class License(models.Model):
-    id_license = models.IntegerField(primary_key=True)
-    id_owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
-    number_license = models.CharField(max_length=10)
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    number = models.CharField(max_length=10)
     type = models.CharField(max_length=10)
     date_issue = models.DateTimeField()
+
