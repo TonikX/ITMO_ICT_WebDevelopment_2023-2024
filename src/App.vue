2b6a9e527134528392d9aaaf63a6e7e767e1f809
godr1se <90646570+godr1se@users.noreply.github.com>
2024-02-26 12:11:16 +0300

    LR4+doc3+doc4

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue';
+</script>
+
+<template>
+  <NavBar />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+
+<style scoped>
+  main {
+    margin-left: 500px;
+    margin-top: 20px;
+    width: calc(100vw - 375px);
+  }
+</style>
---
+++ src/components/AllBooks.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>All Books</h1>
+    <hr class="h">
+    <div v-for="book in Books" :key="book.id">
+        <h3> {{ book.name }}</h3>
+        <h4>{{ book.author }}</h4>
+        <p>{{ book.publisher }}, {{ book.publishing_year }}</p>
+        <p>cipher: {{ book.cipher }}</p>
+        <hr class="h">
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            Books: [],
+        }
+    },
+    mounted () {
+        axios.get('http://127.0.0.1:8000/api/books/all')
+                .then(response => {
+                    this.Books = response.data;
+                })
+                .catch(error => {
+                    console.log('Something went wrong :(')
+                })
+    }
+}
+</script>
+<style scoped>
+  .h {
+    width: 619px; /* Установка ширины линии на 50% ширины родительского контейнера */
+    margin: 10px 0px; /* Центрирование линии по горизонтали */
+  }
+</style>
