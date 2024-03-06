1af907b587eea4604f44e0530f3ff021ae561aaa
Puroblast <Puroblast97@mail.ru>
2024-03-06 17:47:46 +0300

    lr 3 created universal serializers

--- students/K33402/laboratory_works/shliakhov_Denis/laboratory_work_3/school/school_app/views.py
+++ students/K33402/laboratory_works/shliakhov_Denis/laboratory_work_3/school/school_app/views.py
@@ -6,186 +6,57 @@ from .models import *
 from .serializers import *
 
 
-class TeacherListView(generics.ListAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+class UniversalListView(generics.ListAPIView):
+    model = None
+    serializer_class = None
     permission_classes = [permissions.IsAuthenticated]
 
-class TeacherRetrieveView(generics.RetrieveAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = self.model.objects.all()
+
+
+class UniversalRetrieveView(generics.RetrieveAPIView):
+    model = None
+    serializer_class = None
     permission_classes = [permissions.IsAuthenticated]
 
-class TeacherCreateView(generics.CreateAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = self.model.objects.all()
+
+
+class UniversalCreateView(generics.CreateAPIView):
+    model = None
+    serializer_class = None
     permission_classes = [permissions.IsAuthenticated]
 
-class TeacherUpdateView(generics.UpdateAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = self.model.objects.all()
+
+
+class UniversalUpdateView(generics.UpdateAPIView):
+    model = None
+    serializer_class = None
     permission_classes = [permissions.IsAuthenticated]
 
-class TeacherDeleteView(generics.DestroyAPIView):
-    queryset = Teacher.objects.all()
-    serializer_class = TeacherSerializer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = self.model.objects.all()
+
+
+class UniversalDeleteView(generics.DestroyAPIView):
+    model = None
+    serializer_class = None
     permission_classes = [permissions.IsAuthenticated]
 
-class GroupListView(generics.ListAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GroupRetrieveView(generics.RetrieveAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GroupCreateView(generics.CreateAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GroupUpdateView(generics.UpdateAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GroupDeleteView(generics.DestroyAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class StudentListView(generics.ListAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class StudentRetrieveView(generics.RetrieveAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class StudentCreateView(generics.CreateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class StudentUpdateView(generics.UpdateAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class StudentDeleteView(generics.DestroyAPIView):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class LessonListView(generics.ListAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class LessonRetrieveView(generics.RetrieveAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class LessonCreateView(generics.CreateAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class LessonUpdateView(generics.UpdateAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class LessonDeleteView(generics.DestroyAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class TeachProcessListView(generics.ListAPIView):
-    queryset = TeachProcess.objects.all()
-    serializer_class = TeachProcessSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class TeachProcessRetrieveView(generics.RetrieveAPIView):
-    queryset = TeachProcess.objects.all()
-    serializer_class = TeachProcessSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class TeachProcessCreateView(generics.CreateAPIView):
-    queryset = TeachProcess.objects.all()
-    serializer_class = TeachProcessSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class TeachProcessUpdateView(generics.UpdateAPIView):
-    queryset = TeachProcess.objects.all()
-    serializer_class = TeachProcessSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class TeachProcessDeleteView(generics.DestroyAPIView):
-    queryset = TeachProcess.objects.all()
-    serializer_class = TeachProcessSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class SubjectRetrieveView(generics.RetrieveAPIView):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class SubjectCreateView(generics.CreateAPIView):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class SubjectUpdateView(generics.UpdateAPIView):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class SubjectDeleteView(generics.DestroyAPIView):
-    queryset = Subject.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GradeListView(generics.ListAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = SubjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GradeRetrieveView(generics.RetrieveAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GradeCreateView(generics.CreateAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GradeUpdateView(generics.UpdateAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class GradeDeleteView(generics.DestroyAPIView):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = self.model.objects.all()
 
 
 class Logout(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         request.user.auth_token.delete()
         return Response(status=status.HTTP_200_OK)
