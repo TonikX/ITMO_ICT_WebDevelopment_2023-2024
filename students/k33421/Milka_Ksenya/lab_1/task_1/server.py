27a0a8c0e7a55ecb04267352003d5bf96106222a
Ksenya Milka <00000087@mail.ru>
2024-03-12 18:08:03 +0300

    add task_1

---
+++ students/k33421/Milka_Ksenya/lab_1/task_1/server.py
@@ -0,0 +1,12 @@
+import socket
+
+from config import BUFF_SIZE, HOST, PORT
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.bind((HOST, PORT))
+    print("Server started")
+
+    while True:
+        data, addr = s.recvfrom(BUFF_SIZE)
+        print(f"Received from {addr}: {data.decode()}")
+        s.sendto(b"Hello, client!", addr)
