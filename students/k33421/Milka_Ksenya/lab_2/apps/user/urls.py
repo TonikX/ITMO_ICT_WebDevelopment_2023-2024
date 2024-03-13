611b7c5557f117c4037e40cc3c223f906a87b91e
Ksenya Milka <00000087@mail.ru>
2024-03-13 06:02:48 +0300

    add info view

--- students/k33421/Milka_Ksenya/lab_2/apps/user/urls.py
+++ students/k33421/Milka_Ksenya/lab_2/apps/user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('info/', views.UserInfoView.as_view(), name='user_info'),
 ]
--- students/k33421/Milka_Ksenya/lab_2/apps/user/views.py
+++ students/k33421/Milka_Ksenya/lab_2/apps/user/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 
 from apps.user.forms import UserRegisterForm, UserLoginForm
 from apps.user.models import Student, Teacher, Roles
@@ -34,3 +35,12 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('login')
+
+
+class UserInfoView(LoginRequiredMixin, TemplateView):
+    template_name = 'user/information.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_info'] = self.request.user
+        return context
